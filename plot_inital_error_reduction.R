b36cd45b331db49ca41867d1e6e53ac79d700642
guilhermepata <pataguilherme@gmail.com>
2022-04-26 20:06:43 +0100

    Update plot_inital_error_reduction.R

--- plot_inital_error_reduction.R
+++ plot_inital_error_reduction.R
@@ -1,27 +1,28 @@
 # effects_num <- effects::effect(term= "Session", mod= model.split)
 
-pred.mm <- ggpredict(model.split, terms = c("Session"))  # this gives overall predictions for the model
+# pred.mm <- ggpredict(model.split, terms = c("Session"))  # this gives overall predictions for the model
 
-pred_frame = as.data.frame(pred.mm)
-pred_frame <-
+
+data.split.initial.error = as.data.frame(ggpredict(model.split, terms = c("Session", "Num [0]", "Group")))
+
+data.split.initial.error <-
   rename(
-    pred_frame,
+    data.split.initial.error,
     Session = x,
-    # Session = group,
-    # Group = facet,
+    Num = group,
   )
 
-# factor = 1.96
-# 
-# coef.split <- coef(summary(model.split))
+data.split$Predicted = predict(model.split)
 
-(p <- ggplot(data.split, aes(x = Session, y = Asym)) +
-    geom_point(data=(data.split[data.split$Num == 0,]), aes(Session, Asym, color=Animal)) +
-    geom_line(data=data.split[data.split$Num == 0,], aes(Session, Asym, color=Animal, group=Animal)) +
-    geom_point(data=pred_frame, aes(x=Session, y=predicted), size=3) +
-    geom_line(data=pred_frame, aes(x=Session, y=predicted, group=1), size= 1) +
-    geom_errorbar(data=pred_frame, aes(x=Session, y=predicted, ymin=conf.low, ymax=conf.high), width=.2,
+
+(p <- ggplot() +
+    geom_point(data=data.split[data.split$Num == 0,], aes(x=Session, y=Asym, color=Animal)) +
+      geom_line(data=data.split[data.split$Num == 0,], aes(x=Session, y=Asym, color=Animal, group=Animal)) +
+    geom_point(data=data.split.initial.error, aes(x=Session, y=predicted), size=3) +
+    geom_line(data=data.split.initial.error, aes(x=Session, y=predicted, group=0), size= 1) +
+    geom_errorbar(data=data.split.initial.error, aes(x=Session, y=predicted, ymin=conf.low, ymax=conf.high), width=.2,
                   position=position_dodge(0.05)) +
+    geom_hline(yintercept=c(0), linetype="dotted") +
     xlim('S1', 'S2', 'S3', 'S4', 'S5')
 )
 

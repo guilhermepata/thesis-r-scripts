36d8916ffb32cd08fbef087163bff232cc642186
guilhermepata <pataguilherme@gmail.com>
2022-04-26 20:06:41 +0100

    Update plot_change_vs_after_effect.R

--- plot_change_vs_after_effect.R
+++ plot_change_vs_after_effect.R
@@ -4,35 +4,73 @@ conf.interval.factor = 1.96 # multiply std. error by this value to get the one w
 
 num.split.trials = 4
 
-model.split.coef <- as.data.frame(coef(summary(model.split)))
-change.over.split.estimate <-   model.split.coef["SessionS5",]$Estimate + 
-                                model.split.coef["Num:SessionS5",]$Estimate *
-                                num.split.trials
-change.over.split.std.error <- sqrt( model.split.coef["SessionS5",]$"Std. Error"^2 +
-                                    (model.split.coef["Num:SessionS5",]$"Std. Error" *
-                                     num.split.trials)^2)
+data.split.initial.error = as.data.frame(ggpredict(model.split, terms = c("Session", "Num [0]")))
+data.split.initial.error <-
+  rename(
+    data.split.initial.error,
+    Session = x,
+    Num = group,
+  )
+data.split.final.error = as.data.frame(ggpredict(model.split, terms = c("Session", "Num [3]")))
+data.split.final.error <-
+  rename(
+    data.split.final.error,
+    Session = x,
+    Num = group,
+  )
 
-model.washout.coef <- as.data.frame(coef(summary(model.washout)))
-after.effect.estimate <- model.washout.coef["(Intercept)",]$Estimate + 
-                         model.washout.coef["SessionS5",]$Estimate
+change.over.split.animals <- filter(data.split, Session=='S5' & Num==3)$Asym - 
+                            filter(data.split, Session=='S1' & Num==0)$Asym
+change.over.split.estimate <-   data.split.final.error[data.split.final.error$Session == 'S5',]$predicted - 
+                                  data.split.initial.error[data.split.final.error$Session == 'S1',]$predicted
+change.over.split.std.error <- sqrt(data.split.final.error[data.split.final.error$Session == 'S5',]$std.error^2 + 
+                                       data.split.initial.error[data.split.final.error$Session == 'S1',]$std.error^2
+                                    )
 
-after.effect.std.error <-sqrt( model.washout.coef["(Intercept)",]$"Std. Error"^2 +
-                              model.washout.coef["SessionS5",]$"Std. Error"^2)
+data.washout.after.effect = as.data.frame(ggpredict(model.washout, terms = c("Session", "Num [4]")))
+data.washout.after.effect <-
+  rename(
+    data.washout.after.effect,
+    Session = x,
+    Num = group,
+  )
+
+after.effect.animals <- filter(data.washout, Session=='S5' & Num==0)$Asym
+after.effect.estimate <- data.washout.after.effect[data.wahsout.after.effect$Session == 'S5']$predicted
+after.effect.std.error <- data.washout.after.effect[data.wahsout.after.effect$Session == 'S5']$std.error
 
 df <- data.frame(Phase = c('Change over split', 'After effect'),
                  Estimate = c(change.over.split.estimate, after.effect.estimate),
                  "Std.Error" = c(change.over.split.std.error, after.effect.std.error)
                  )
 
+df.animals = data.frame(Phase = c(), Estimate = c(), Animal = c())
+for (animal in unique(data.split$Animal)) {
+  df.aux = data.frame(Phase = c('Change over split'),
+                      Estimate = c(filter(data.split, Session=='S5' & Num==3 & Animal==animal)$Asym - 
+                        filter(data.split, Session=='S1' & Num==0 & Animal==animal)$Asym),
+                      Animal = c(animal)
+  )
+  df.animals <- rbind(df.animals, df.aux)
+}
+for (animal in unique(data.washout$Animal)) {
+  df.aux = data.frame(Phase = c('After effect'),
+                      Estimate = c(filter(data.split, Session=='S5' & Num==0 & Animal==animal)$Asym),
+                      Animal = c(animal)
+  )
+  df.animals <- rbind(df.animals, df.aux)
+}
 
 (p <- ggplot() +
-    # geom_point(data=pred_frame, aes(x=Session, y=predicted), size=3) +
-    # geom_line(data=pred_frame, aes(x=Session, y=predicted), size= 1) +
+    geom_point(data=df.animals, aes(x=Phase, y=Estimate, group=Animal, color=Animal)) +
+    geom_line(data=df.animals, aes(x=Phase, y=Estimate, group=Animal, color=Animal)) +
     geom_point(data=df, aes(x=Phase, y=Estimate), size  = 3) +
     geom_errorbar(data=df, aes(x=Phase, y=Estimate, ymin=Estimate-Std.Error, ymax=Estimate+Std.Error), width=.2,
                   position=position_dodge(0.05)) +
+    geom_line(data=df, aes(x=Phase, y=Estimate, group=0), size  = 1) +
+    geom_hline(yintercept=c(0), linetype="dotted") +
     xlim('Change over split', 'After effect')
 )
 
 p+labs(title="Change over split vs after effect", x="", y = "Step length asymmetry (mm)")+
-  theme_classic()
+  theme_classic()+theme(legend.position="none")

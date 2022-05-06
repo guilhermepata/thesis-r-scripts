ee7ff67328f36fefbc5c0c9e298aac7eb472e694
guilhermepata <pataguilherme@gmail.com>
2022-05-06 14:32:58 +0100

    Update stat_tests.R

--- stat_tests.R
+++ stat_tests.R
@@ -1,15 +1,28 @@
 ## Initial error
 
+{if (name == 'Exp3' || name == 'Exp5') {
+  initial.error.test.at = list(Session=c('S1'), Num=0)
+}
+
 (initial.error.test = summary(emmeans(
                       model.split, pairwise ~ Group, 
-                      at=list(Session=c('S1'), Num=0))$contrasts))
+                      at=initial.error.test.at))$contrasts)
+}
+
 
 ## Learning rate
 
+{if (name == 'Exp3') {
+  learning.rate.test.at = list(Session=c('S1', 'S2', 'S3', 'S4'))
+} else if (name == 'Exp5') {
+  learning.rate.test.at = list(Session=c('S1'))
+}
+  
 (learning.rate.test = summary(emtrends(
                                     model.split, pairwise ~ Group, 
                                     var = "Num",
-                                    at=list(Session=c('S1', 'S2', 'S3', 'S4')))$contrasts))
+                                    at=learning.rate.test.at)))
+}
 
 ## Session 6
 # initial error
@@ -27,23 +40,37 @@
   at=list(Session=c('S6'), Num=0))$contrasts))
 
 
+## Washouts
+(washouts.test = summary(emmeans(
+                            model.washout, pairwise ~ Group,
+                            at = list(Session = c('S1', 'S2', 'S3', 'S4', 'S5'), Num=0)
+                            )))
+
+
 ## Switch group
+
+{if (name == 'Exp3') {
+  switch.group.test.at = list(Session=c('S5', 'S4', 'S3', 'S2', 'S1'), Num=4, Group='NotAtaxic:Switch')
+} else if (name == 'Exp5') {
+  switch.group.test.at = list(Session=c('S5', 'S4', 'S3', 'S2', 'S1'), Num=0, Group='NotAtaxic:Switch')
+}
+  
 (error.reduction.test = summary(emmeans(
   model.split, pairwise ~ Session, 
-  at=list(Session=c('S5', 'S4', 'S3', 'S2', 'S1'), Num=0, Group='NotAtaxic:Switch'))$contrasts))
+  at=switch.group.test.at)$contrasts))
 (after.effect.test = summary(emmeans(
   model.washout, ~ Session, 
-  at=list(Session=c('S5', 'S4', 'S3', 'S2', 'S1'), Num=0, Group='NotAtaxic:Switch'))))
+  at=switch.group.test.at)))
 (after.effect.test = summary(emmeans(
   model.washout, identity ~ Session, 
-  at=list(Session=c('S5', 'S4', 'S3', 'S2', 'S1'), Num=0, Group='NotAtaxic:Switch'))))
+  at=switch.group.test.at)))
 (washout.test = summary(emmeans(
   model.washout, identity ~ Session, 
-  at=list(Session=c('S5', 'S4', 'S3', 'S2', 'S1'), Num=4, Group='NotAtaxic:Switch'))))
+  at=switch.group.test.at)))
 (baseline.test = summary(emmeans(
-  model.washout, identity ~ Session, 
-  at=list(Session=c('S5', 'S4', 'S3', 'S2', 'S1'), Num=0, Group='NotAtaxic:Switch'))))
-
+  model.baseline, identity ~ Session, 
+  at=switch.group.test.at)))
+}
 
 (conserved.error.test = summary(emmeans(
   model.split, pairwise ~ Session * Num, 
@@ -52,3 +79,18 @@
 (change.over.split.test = as.data.frame(emmeans(
   model.split, pairwise ~ Session * Num, 
   at=list(Session=c('S5', 'S1'), Num=c(3, 0), Group='NotAtaxic:Switch'))$contrasts))
+
+
+
+## NoSwitch group
+
+(change.over.split.test2 = as.data.frame(emmeans(
+  model.split, ~ Group | Session | Num, 
+  at=list(Session=c('S4', 'S1'), Num=c(3, 0)))))
+
+(after.effect.test2 = as.data.frame(emmeans(
+  model.washout, ~ Group | Session | Num, 
+  at=list(Session=c('S4'), Num=0))))
+
+
+

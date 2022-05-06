7b11f6547454da9808a8bb87edab095adb2b7ffc
guilhermepata <pataguilherme@gmail.com>
2022-05-06 14:33:02 +0100

    Update build_spatiotemporal_model.R

--- build_spatiotemporal_model.R
+++ build_spatiotemporal_model.R
@@ -1,16 +1,19 @@
 source("build_model.R")
 library(MuMIn)
 
-model.equation.spatiotemporal = 'Asym ~ Group *(coo + double_support ) + (1|Animal) + (1|Phase)'
+data.zscored = data.split
+data.zscored[10:17] <- scale(data.zscored[10:17])
 
-model.spatiotemporal <-lmer(model.equation.spatiotemporal, data=data.total_frame, REML= "false")
+model.equation.spatiotemporal = 'Asym ~ Group *(coo + double_support + stance_speed + swing_length) + (1|Animal) + (1|Session)'
 
-data.split <- data_frame[data_frame$Phase == "Split",]
-data.total2 <- cbind(data.total_frame, AsymFit = predict(model.spatiotemporal))
+model.spatiotemporal <-lmer(model.equation.spatiotemporal, data=data.zscored, REML= "false")
+
+# data.split <- data_frame[data_frame$Phase == "Split",]
+data.zscored2 <- cbind(data.zscored, AsymFit = predict(model.spatiotemporal))
 
 # data.split.summary <- cbind(data.split, AsymFit = predict(model.spatiotemporal, newdata=data.split.summary, re.form = ~0))
 
-data.summary2 = summarise(group_by(data.total2, Trial, Num, Session, Group), 
+data.summary2 = summarise(group_by(data.zscored2, Trial, Num, Session, Group), 
                                 MeanFit = mean(AsymFit), 
                                 MedianFit = median(AsymFit), 
                                 Mean = mean(Asym),
@@ -18,7 +21,7 @@ data.summary2 = summarise(group_by(data.total2, Trial, Num, Session, Group),
 
 # data.split.summary2 = summarise.predict(data.split, model.spatiotemporal, Group, Session, Num, Trial)
 
-(p <- ggplot(data=data.summary2, aes(x=Trial)) + 
+(p <- ggplot(data=data.summary2, aes(x=Trial)) +
     # facet_wrap(vars(Session), nrow = 1) +
     geom_point(aes(y=Mean, color=Group, shape=Group), alpha=0.3) +
     geom_line(aes(y=Mean, color=Group, shape=Group, group=Group), alpha=0.3) +
@@ -42,6 +45,7 @@ ms <- modelsummary(model.spatiotemporal, output='data.frame', stars=TRUE, gof_ma
 r.squaredGLMM(model.spatiotemporal)
 
 (means.test = emmeans(model.spatiotemporal, pairwise ~ Group, at = list(Phase='Split', coo=0, double_support=0)))
-(spatial.trend.test = emtrends(model.spatiotemporal, pairwise ~ Group, var = "coo", at=list(Phase='Split', double_support=0)))
-(temporal.trend.test = emtrends(model.spatiotemporal, pairwise ~ Group, var = "double_support", at=list(Phase='Split', coo=0)))
-
+(spatial.trend.test = emtrends(model.spatiotemporal, pairwise ~ Group, var = "coo", at=list(Phase='Split')))
+(temporal.trend.test = emtrends(model.spatiotemporal, pairwise ~ Group, var = "double_support", at=list(Phase='Split')))
+(stl.trend.test = emtrends(model.spatiotemporal, pairwise ~ Group, var = "swing_length", at=list(Phase='Split')))
+(sts.trend.test = emtrends(model.spatiotemporal, pairwise ~ Group, var = "stance_speed", at=list(Phase='Split')))

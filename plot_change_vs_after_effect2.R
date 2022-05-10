f41bafc2425b012a61a7b3ec2d53023753add38c
guilhermepata <pataguilherme@gmail.com>
2022-05-10 16:53:22 +0100

    Update plot_change_vs_after_effect2.R

--- plot_change_vs_after_effect2.R
+++ plot_change_vs_after_effect2.R
@@ -17,23 +17,81 @@ plot.change <- function(df, df.animals) {
 
 ### plot switch group
 
-num.split.trials.switch = max(unique(filter(data.split, Protocol == "Switch", Session == 'S5')$Num))
-
-predict.change.over.split.switch <- function(mod) {
-  first.error.df = data.frame(Num = 0, Group = "NotAtaxic:Switch", Session = "S1")
-  final.error.df = data.frame(Num = num.split.trials.switch, Group = "NotAtaxic:Switch", Session = "S5")
-  return(predict(mod, newdata=final.error.df, re.form = ~0) - predict(mod, newdata=first.error.df, re.form = ~0))
+make_change_df <- function(data.summary, 
+                           model.split, 
+                           model.washout, 
+                           group,
+                           sessions.split = c('S1', 'S5')) {
+  
+  data.summary2 = filter(data.summary, Group == group)
+  
+  first.trial = min(filter(data.summary2, Phase == 'Split', 
+                           Session == sessions.split[[1]])$Trial)
+  last.trial = max(filter(data.summary2, Phase == 'Split',
+                          Session == sessions.split[[2]])$Trial)
+  washout.trial = last.trial + 1
+  
+  first.trial.row = filter(data.summary2, Trial==first.trial)
+  last.trial.row = filter(data.summary2, Trial==last.trial)
+  washout.row = filter(data.summary2, Trial==washout.trial)
+  
+  first.num = first.trial.row$Num
+  first.session = first.trial.row$Session
+  
+  last.num = last.trial.row$Num
+  last.session = last.trial.row$Session
+  
+  washout.num = washout.row$Num
+  washout.session = washout.row$Session
+  
+  s.split = summary(emmeans(
+      model.split,
+      revpairwise ~ Num * Session * Group,
+      at = list(
+        Session = c(first.session, last.session),
+        Num = c(first.num, last.num),
+        Group = group
+      )
+  )$contrasts, infer=TRUE)
+  
+  s.split = filter(s.split, contrast == paste(last.num, 
+                                              last.session,
+                                              group,
+                                              '-',
+                                              first.num,
+                                              first.session,
+                                              group,
+                                              sep=' '))
+  
+  s.washout = summary(emmeans(
+    model.split,
+    identity ~ Num * Session * Group,
+    at = list(
+      Session = washout.session,
+      Num = washout.num,
+      Group = group
+    )
+  )$contrasts, infer=TRUE)
+  
+  df <- data.frame(
+    Phase = c('Change over split', 'After effect'),
+    Fit = c(s.split$estimate, s.washout$estimate),
+    Lower = c(s.split$lower.CL, s.washout$lower.CL),
+    Upper = c(s.split$upper.CL, s.washout$upper.CL),
+    P.value = c(s.split$p.value, s.washout$p.value)
+  )
+  
+  return(df)
+  
 }
-change.over.split.switch <- statBounded(model.split, predict.change.over.split.switch)
 
-predict.after.effect.switch <- function(mod) {
-  after.effect.df = data.frame(Num = 0, Group = "NotAtaxic:Switch", Session = "S5")
-  return(predict(mod, newdata=after.effect.df, re.form = ~0))
-}
-after.effect.switch <- statBounded(model.washout, predict.after.effect.switch)
-
-df.switch <- cbind(Phase = c('Change over split', 'After effect'), rbind(change.over.split.switch, after.effect.switch))
-
+df.switch <- make_change_df(data.summary,
+                            model.split,
+                            model.washout,
+                            group = 'NotAtaxic:Switch',
+                            sessions.split = c('S1','S5'))
+  
+  
 df.animals.switch = data.frame(Phase = c(), Asym = c(), Animal = c())
 for (animal in unique(filter(data.split, Protocol=='Switch')$Animal)) {
   df.aux = data.frame(Phase = c('Change over split'),

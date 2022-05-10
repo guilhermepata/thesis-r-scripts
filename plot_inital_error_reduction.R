dd7920a3e2b212b0709130a0b45a66d0c6a5ddd0
guilhermepata <pataguilherme@gmail.com>
2022-05-10 16:53:25 +0100

    Update plot_inital_error_reduction.R

--- plot_inital_error_reduction.R
+++ plot_inital_error_reduction.R
@@ -1,59 +1,122 @@
 source("build_model.R")
 source("my_functions.R")
 
-data.initial.error = filter(data.split, Num==0)
-
-to.predict = as.data.frame(data.initial.error %>%
-  group_by(Num, Group, Session) %>%
-  summarise())
-
-to.predict$Prediction = predict(model.split, newdata=to.predict, re.form = ~0)
-
-predFun <- function(x) predict(x,newdata=to.predict,re.form=NA)
-bb <- bootMer(model.split,
-              FUN=predFun,
-              nsim=200)  
-bb_ci <- as.data.frame(t(apply(bb$t,2,quantile,c(0.025,0.975))))
-names(bb_ci) <- c("lwr","upr")
-to.predict <- cbind(to.predict, bb_ci)
-
-# to.predict = cbind(to.predict, predictInterval(model.split, newdata=to.predict, which="fixed"))
-
-
-### Plot switch group
-
-to.predict.switch <- filter(to.predict, Group == 'NotAtaxic:Switch')
-
-plot.errors.switch <- ggplot() +
-    geom_point(data=filter(data.split, Protocol=='Switch', Num==0), aes(x=Session, y=Asym, color=Animal), alpha = 0.2) +
-    geom_line(data=filter(data.split, Protocol=='Switch', Num==0), aes(x=Session, y=Asym, color=Animal, group=Animal), alpha = 0.2) +
-    geom_point(data=to.predict.switch, aes(x=Session, y=Prediction), size=3) +
-    geom_line(data=to.predict.switch, aes(x=Session, y=Prediction, group=0), size= 1) +
-    geom_errorbar(data=to.predict.switch, aes(x=Session, y=Prediction, ymin=lwr, ymax=upr), width=.2,
-        position=position_dodge(0.05)) +
-    geom_hline(yintercept=c(0), linetype="dotted") +
-    xlim('S1', 'S2', 'S3', 'S4', 'S5') +
-     theme_classic() + theme(legend.position="none") +
-     labs(x="Session", y = "Step length asymmetry (mm)")
-
-### Plot noswitch group
-
-to.predict.noswitch <- filter(to.predict, Group == 'NotAtaxic:NoSwitch')
-
-plot.errors.noswitch  <- ggplot() +
-    geom_point(data=filter(data.split, Protocol=='NoSwitch', Num==0), aes(x=Session, y=Asym, color=Animal), alpha = 0.2) +
-    geom_line(data=filter(data.split, Protocol=='NoSwitch', Num==0), aes(x=Session, y=Asym, color=Animal, group=Animal), alpha = 0.2) +
-    geom_point(data=to.predict.noswitch, aes(x=Session, y=Prediction), size=3) +
-    geom_line(data=to.predict.noswitch, aes(x=Session, y=Prediction, group=0), size= 1) +
-    geom_errorbar(data=to.predict.noswitch, aes(x=Session, y=Prediction, ymin=lwr, ymax=upr), width=.2,
-                  position=position_dodge(0.05)) +
-    geom_hline(yintercept=c(0), linetype="dotted") +
-    xlim('S1', 'S2', 'S3', 'S4') +
-    theme_classic() + theme(legend.position="none") +
-    labs(x="Session", y = "Step length asymmetry (mm)")
+plot.initial.error <- function(data.split,
+                               data.split.summary,
+                               group, 
+                               color='#619CFF', 
+                               xlim=c('S1', 'S2', 'S3', 'S4', 'S5')) {
+  
+  color.light <- lighten(color)
+  
+  p = ggplot() +
+  geom_point(data=filter(data.split, Group==group, Num==0), 
+             aes(x=Session, y=Asym), 
+             alpha = 0.5,
+             color = color.light) +
+  geom_line(data=filter(data.split, Group==group, Num==0), 
+            aes(x=Session, y=Asym, group=Animal), 
+            alpha = 0.5,
+            color = color.light) +
+  
+  geom_point(data=filter(data.split.summary, Group==group, Num==0), 
+             aes(x=Session, y=Fit), 
+             size=3, 
+             color=color) +
+  geom_line(data=filter(data.split.summary, Group==group, Num==0), 
+            aes(x=Session, y=Fit, group=0), 
+            size= 1, 
+            color=color) +
+  geom_errorbar(data=filter(data.split.summary, Group==group, Num==0), 
+                aes(x=Session, y=Fit, ymin=Lower, ymax=Upper), 
+                width=.2,
+                position=position_dodge(0.05),
+                color=color) +
+    
+  geom_hline(yintercept=c(0), linetype="dashed", alpha=0.5) +
+  xlim(xlim) +
+  theme_classic() + theme(legend.position="none") +
+  labs(x="Session", y = "Step length asymmetry (mm)")
+  
+  return(p)
+}
 
 
-# ylim = equal_y_limits(plot.errors.noswitch, plot.errors.switch)
-# 
-# plot.errors.noswitch = plot.errors.noswitch + ylim(ylim)
-# plot.errors.switch = plot.errors.switch + ylim(ylim)
+if (name=='Exp3') {
+
+  ### Plot switch group
+  {
+  group = 'NotAtaxic:Switch'
+  color = get_group_color(group)
+  
+  (plot.errors.switch <- plot.initial.error(data.split, data.split.summary,
+                                           group = group,
+                                           color = color))
+  }
+  
+  ### Plot noswitch group
+  {
+    group = 'NotAtaxic:NoSwitch'
+    color = get_group_color(group)
+    xlim = c('S1', 'S2', 'S3', 'S4')
+    
+    (plot.errors.noswitch <- plot.initial.error(data.split, data.split.summary,
+                                              group = group,
+                                              color = color,
+                                              xlim = xlim))
+  }
+}
+
+if (name == 'Exp5') {
+  
+  ### Plot switch group
+  {
+    group = 'NotAtaxic:Switch'
+    color = get_group_color(group)
+    
+    (plot.errors.switch <- plot.initial.error(data.split, data.split.summary,
+                                              group = group,
+                                              color = color))
+  }
+  
+  ### Plot noswitch group
+  {
+    group = 'NotAtaxic:NoSwitch'
+    color = get_group_color(group)
+    xlim = c('S1', 'S2', 'S3', 'S4')
+    
+    (plot.errors.noswitch <- plot.initial.error(data.split, data.split.summary,
+                                                group = group,
+                                                color = color,
+                                                xlim = xlim))
+  }
+}
+
+if (name=='Exp4') {
+  
+  ### Plot switch group
+  {
+    group = 'NotAtaxic:Switch'
+    color = get_group_color(group)
+    xlim = c('S1', 'S2', 'S3', 'S5')
+    
+    (plot.errors.switch <- plot.initial.error(data.split, data.split.summary,
+                                              group = group,
+                                              color = color,
+                                              xlim = xlim))
+  }
+  
+  ### Plot ataxic group
+  {
+    group = 'Ataxic:Switch'
+    color = get_group_color(group)
+    xlim = c('S1', 'S2', 'S3', 'S5')
+    
+    (plot.errors.ataxic <- plot.initial.error(data.split, data.split.summary,
+                                                group = group,
+                                                color = color,
+                                                xlim = xlim))
+  }
+}
+
+

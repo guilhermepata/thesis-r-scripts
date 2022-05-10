d867c81bf03581610dd3dd41e1eff610eedca029
guilhermepata <pataguilherme@gmail.com>
2022-05-10 17:39:18 +0100

    Update fig 2 plots

--- join_fig2_plots.R
+++ join_fig2_plots.R
@@ -1,22 +1,35 @@
 library(cowplot)
 library(patchwork)
 is.built = FALSE
-name = 'Exp3'
+name = 'Exp5'
 source("my_functions.R")
 source("plot_inital_error_reduction.R")
+source("plot_change_vs_after_effect2.R")
 
 if( name == 'Exp4') {
   
   plotlist = list(plot.errors.switch,
                   plot.errors.ataxic
   )
-  
-} else {
+  now = 1
+} else if (name == 'Exp5') {
+  # plotlist = list(plot.errors.noswitch,
+  #                 plot.errors.switch)
+  # nrow = 1
   plotlist = list(plot.errors.noswitch,
-                  plot.errors.switch)
+                  plot.errors.switch,
+                  plot.change.noswitch,
+                  plot.change.switch)
+  nrow = 2
+} else if (name == 'Exp3') {
+  plotlist = list(plot.errors.noswitch,
+                  plot.errors.switch,
+                  plot.change.noswitch,
+                  plot.change.switch)
+  nrow = 2
 }
 
-for (i in 1:(length(plotlist)%/%2)) {
+for (i in (1:(length(plotlist)%/%2))*2-1) {
   delta = 1
   list[a, b] = set_equal_y_lims(plotlist[[i]], plotlist[[i+delta]])
   plotlist[[i]] = a
@@ -31,7 +44,7 @@ for (i in 1:length(plotlist)) {
     )
 }
 
-(p = cowplot::plot_grid(plotlist = plotlist, nrow = 1, labels = 'AUTO', scale = .9))
+(p = cowplot::plot_grid(plotlist = plotlist, nrow = nrow, labels = 'AUTO', scale = .9))
 
 ggsave(paste("plots/", name, "_fig2_plot", ".pdf", sep=""), width = 8.27, height = 5.83, device = cairo_pdf)
 
--- plot_change_vs_after_effect2.R
+++ plot_change_vs_after_effect2.R
@@ -1,21 +1,36 @@
 source("build_model.R")
 source("my_functions.R")
 
-plot.change <- function(df, df.animals) {
+plot.change <- function(df, df.animals, color='#619CFF') {
+  
+  color.light = lighten(color)
+  
   ggplot() +
-    geom_point(data=df.animals, aes(x=Phase, y=Asym, group=Animal, color=Animal), alpha = 0.2) +
-    geom_line(data=df.animals, aes(x=Phase, y=Asym, group=Animal, color=Animal), alpha = 0.2) +
-    geom_point(data=df, aes(x=Phase, y=Fit), size  = 3) +
-    geom_errorbar(data=df, aes(x=Phase, y=Fit, ymin=Lower, ymax=Upper), width=.1,
-                  position=position_dodge(0.05)) +
-    geom_line(data=df, aes(x=Phase, y=Fit, group=0), size  = 1) +
-    geom_hline(yintercept=c(0), linetype="dotted") +
+    geom_point(data=df.animals, aes(x=Phase, y=Asym, group=Animal), 
+               alpha = 0.5, 
+               color = color.light) +
+    geom_line(data=df.animals, aes(x=Phase, y=Asym, group=Animal),
+              alpha = 0.5,
+              color = color.light) +
+    
+    geom_point(data=df, aes(x=Phase, y=Fit), 
+               size = 3,
+               color = color) +
+    geom_errorbar(data=df, aes(x=Phase, y=Fit, ymin=Lower, ymax=Upper), 
+                  width=.1,
+                  position=position_dodge(0.05),
+                  color=color) +
+    geom_line(data=df, aes(x=Phase, y=Fit, group=0), 
+              size  = 1,
+              color=color,
+              linetype = "dashed") +
+    
+    geom_hline(yintercept=c(0), linetype="dashed", alpha = 0.5) +
+    
     scale_x_discrete(limits=c('Change over split', 'After effect'), labels=c("Change over split" = "COS", "After effect" = "AE")) +
     theme_classic() + theme(legend.position="none") +
     labs(x=element_blank(), y = "Step length asymmetry (mm)")
 }
-
-### plot switch group
 
 make_change_df <- function(data.summary, 
                            model.split, 
@@ -64,7 +79,7 @@ make_change_df <- function(data.summary,
                                               sep=' '))
   
   s.washout = summary(emmeans(
-    model.split,
+    model.washout,
     identity ~ Num * Session * Group,
     at = list(
       Session = washout.session,
@@ -85,71 +100,147 @@ make_change_df <- function(data.summary,
   
 }
 
+make_change_df_animals <- function(data.split,
+                                   data.washout,
+                                   data.summary, 
+                                   model.split, 
+                                   model.washout, 
+                                   group,
+                                   sessions.split = c('S1', 'S5')) {
+  
+  data.summary2 = filter(data.summary, Group == group)
+  
+  first.trial = min(filter(data.summary2, Phase == 'Split', 
+                           Session == sessions.split[[1]])$Trial)
+  last.trial = max(filter(data.summary2, Phase == 'Split',
+                          Session == sessions.split[[2]])$Trial)
+  washout.trial = last.trial + 1
+  
+  first.trial.row = filter(data.summary2, Trial==first.trial)
+  last.trial.row = filter(data.summary2, Trial==last.trial)
+  washout.row = filter(data.summary2, Trial==washout.trial)
+  
+  first.num = first.trial.row$Num
+  first.session = first.trial.row$Session
+  
+  last.num = last.trial.row$Num
+  last.session = last.trial.row$Session
+  
+  washout.num = washout.row$Num
+  washout.session = washout.row$Session
+  
+  df.animals = data.frame(Phase = c(), Asym = c(), Animal = c())
+  
+  for (animal in unique(filter(data.split, Group==group)$Animal)) {
+    df.aux = data.frame(Phase = c('Change over split'),
+                        Asym = c(filter(data.split, 
+                                        Session==last.session & 
+                                          Num==last.num & 
+                                          Animal==animal)$Asym - 
+                                   filter(data.split, 
+                                          Session==first.session & 
+                                            Num==first.num & 
+                                            Animal==animal)$Asym),
+                        Animal = c(animal)
+    )
+    df.animals = rbind(df.animals, df.aux)
+  }
+  
+  for (animal in unique(filter(data.washout, Group==group)$Animal)) {
+    df.aux = data.frame(Phase = c('After effect'),
+                        Asym = c(filter(data.washout, 
+                                        Session==washout.session &
+                                          Num==washout.num & 
+                                          Animal==animal)$Asym),
+                        Animal = c(animal)
+    )
+    df.animals = rbind(df.animals, df.aux)
+  }
+  
+  return(df.animals)
+  
+}
+
+if (name != 'Exp4'){
+
+### plot switch group
+
+group = 'NotAtaxic:Switch'
+color = get_group_color(group)
+  
 df.switch <- make_change_df(data.summary,
                             model.split,
                             model.washout,
-                            group = 'NotAtaxic:Switch',
+                            group = group,
                             sessions.split = c('S1','S5'))
-  
-  
-df.animals.switch = data.frame(Phase = c(), Asym = c(), Animal = c())
-for (animal in unique(filter(data.split, Protocol=='Switch')$Animal)) {
-  df.aux = data.frame(Phase = c('Change over split'),
-                      Asym = c(filter(data.split, Session=='S5' & Num==num.split.trials.switch & Animal==animal)$Asym - 
-                                     filter(data.split, Session=='S1' & Num==0 & Animal==animal)$Asym),
-                      Animal = c(animal)
-  )
-  df.animals.switch = rbind(df.animals.switch, df.aux)
-}
-for (animal in unique(filter(data.washout, Protocol=='Switch')$Animal)) {
-  df.aux = data.frame(Phase = c('After effect'),
-                      Asym = c(filter(data.washout, Session=='S5' & Num==0 & Animal==animal)$Asym),
-                      Animal = c(animal)
-  )
-  df.animals.switch = rbind(df.animals.switch, df.aux)
-}
 
-plot.change.switch <- plot.change(df.switch, df.animals.switch)
+  
+df.animals.switch = make_change_df_animals(data.split,
+                                           data.washout,
+                                           data.summary,
+                                           model.split,
+                                           model.washout,
+                                           group = group,
+                                           sessions.split = c('S1','S5'))
+
+plot.change.switch <- plot.change(df.switch, df.animals.switch, color=color)
 
 
 ### plot noswitch group
+  
+  if (name == 'Exp3') {
+    sessions.split = c('S1', 'S4')
+  } else if (name == 'Exp5') {
+    sessions.split = c('S1', 'S5')
+  }
+  
+group = 'NotAtaxic:NoSwitch'
+color = get_group_color(group)
 
-num.split.trials.noswitch = max(unique(filter(data.split, Protocol == "NoSwitch", Session == 'S4')$Num))
+df.noswitch <- make_change_df(data.summary,
+                            model.split,
+                            model.washout,
+                            group = group,
+                            sessions.split = sessions.split)
 
-predict.change.over.split.noswitch <- function(mod) {
-  first.error.df = data.frame(Num = 0, Group = "NotAtaxic:NoSwitch", Session = "S1")
-  final.error.df = data.frame(Num = num.split.trials.noswitch, Group = "NotAtaxic:NoSwitch", Session = "S4")
-  return(predict(mod, newdata=final.error.df, re.form = ~0) - predict(mod, newdata=first.error.df, re.form = ~0))
-}
-change.over.split.noswitch <- statBounded(model.split, predict.change.over.split.noswitch)
 
-predict.after.effect.noswitch <- function(mod) {
-  after.effect.df = data.frame(Num = 0, Group = "NotAtaxic:NoSwitch", Session = "S4")
-  return(predict(mod, newdata=after.effect.df, re.form = ~0))
-}
-after.effect.noswitch <- statBounded(model.washout, predict.after.effect.noswitch)
+df.animals.noswitch = make_change_df_animals(data.split,
+                                           data.washout,
+                                           data.summary,
+                                           model.split,
+                                           model.washout,
+                                           group = group,
+                                           sessions.split = sessions.split)
 
-df.noswitch <- cbind(Phase = c('Change over split', 'After effect'), rbind(change.over.split.noswitch, after.effect.noswitch))
-
-df.animals.noswitch = data.frame(Phase = c(), Asym = c(), Animal = c())
-for (animal in unique(filter(data.split, Protocol=='NoSwitch')$Animal)) {
-  df.aux = data.frame(Phase = c('Change over split'),
-                      Asym = c(filter(data.split, Session=='S4' & Num==num.split.trials.noswitch & Animal==animal)$Asym - 
-                                filter(data.split, Session=='S1' & Num==0 & Animal==animal)$Asym),
-                      Animal = c(animal)
-  )
-  df.animals.noswitch = rbind(df.animals.noswitch, df.aux)
-}
-for (animal in unique(filter(data.washout, Protocol=='NoSwitch')$Animal)) {
-  df.aux = data.frame(Phase = c('After effect'),
-                      Asym = c(filter(data.washout, Session=='S4' & Num==0 & Animal==animal)$Asym),
-                      Animal = c(animal)
-  )
-  df.animals.noswitch = rbind(df.animals.noswitch, df.aux)
+plot.change.noswitch <- plot.change(df.noswitch, df.animals.noswitch, color=color)
 }
 
-plot.change.noswitch <- plot.change(df.noswitch, df.animals.noswitch)
+if (name == 'Exp4') {
+  
+  ## plot not ataxic
+  
+  group = 'NotAtaxic:NoSwitch'
+  color = get_group_color(group)
+  
+  df.switch <- make_change_df(data.summary,
+                              model.split,
+                              model.washout,
+                              group = 'NotAtaxic:Switch',
+                              sessions.split = c('S1','S2'))
+  
+  
+  df.animals.switch = make_change_df_animals(data.split,
+                                             data.washout,
+                                             data.summary,
+                                             model.split,
+                                             model.washout,
+                                             group = 'NotAtaxic:Switch',
+                                             sessions.split = c('S1','S2'))
+  
+  plot.change.switch <- plot.change(df.switch, df.animals.switch, color=color)
+  
+  ## plot ataxic ???
+  
+}
 
-
-### Do some stats
 

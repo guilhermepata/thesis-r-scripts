a5ce7eefe8f2724211c8892e7f66ad7d40749690
guilhermepata <pataguilherme@gmail.com>
2022-05-25 12:02:16 +0100

    Update join_protocol_plots.R

--- join_protocol_plots.R
+++ join_protocol_plots.R
@@ -1,16 +1,25 @@
 library(magick)
+library(cowplot)
 library(dplyr)
 library(forcats)
+library(ggplot2)
 source("plot_protocol.R")
 
 img <-   image_read("./plots/split_belt_setup.png") %>%
-          image_resize("570x380")
+  image_resize("570x380")
 
-setup = ggdraw() + 
-  draw_image(img) 
+setup = ggdraw() +
+  draw_image(img)
 
-plotlist = list(setup, plot.short.protocol,
-                plot.exp3.noswitch.protocol, plot.exp3.switch.protocol, plot.exp5.noswitch.protocol, plot.exp5.switch.protocol, plot.exp4.protocol)
+plotlist = list(
+  setup,
+  plot.short.protocol,
+  plot.exp3.noswitch.protocol,
+  plot.exp3.switch.protocol,
+  plot.exp5.noswitch.protocol,
+  plot.exp5.switch.protocol,
+  plot.exp4.protocol
+)
 
 for (i in 1:length(plotlist)) {
   plotlist[[i]] = plotlist[[i]] +
@@ -19,23 +28,31 @@ for (i in 1:length(plotlist)) {
 
 
 toprow = cowplot::plot_grid(
-  plotlist = plotlist[c(1,2)],
+  plotlist = plotlist[c(1, 2)],
   nrow = 1,
   labels = 'AUTO',
   align = "Vh"
 )
 
 bottomrow = cowplot::plot_grid(
-  plotlist = plotlist[c(3,4,5,6,7)],
+  plotlist = plotlist[c(3, 4, 5, 6, 7)],
   nrow = 5,
-  labels = c('C','D','E','F','G'),
+  labels = c('C', 'D', 'E', 'F', 'G'),
   align = "Vh"
 )
 
 p = cowplot::plot_grid(
-  toprow, bottomrow,
+  toprow,
+  bottomrow,
   nrow = 2,
   align = "vh",
   rel_heights = c(1, 4)
 )
 
+
+ggsave(plot = p,
+  paste("plots/", "protocol_plot", ".png", sep = ""),
+  # device = cairo_pdf,
+  width = 8.27,
+  height = 11.69
+)
